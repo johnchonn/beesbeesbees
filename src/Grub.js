2417ff3f03ff5035929704a00396079cfef4f2df
John Chon <johnchon94@gmail.com>
2022-02-28 12:28:32 -0800

    BeesBeesBees completeed

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,13 @@
 var Grub = function() {
+  // add an age property
+  // add a color property
+  // add a food property
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
 
+// create a eat method
+Grub.prototype.eat = function() {
+
+};
